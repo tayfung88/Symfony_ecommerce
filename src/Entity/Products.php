e0ec556c77cdf302d3967b8b7243a6944015bf94
tayfung88 <tayfungg88@gmail.com>
2024-01-30 09:51:52 +0100

    Apip

--- src/Entity/Products.php
+++ src/Entity/Products.php
@@ -2,11 +2,13 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\ProductsRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ProductsRepository::class)]
+#[ApiResource()]
 class Products
 {
     #[ORM\Id]
